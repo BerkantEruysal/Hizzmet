bd502949ee9b26b15f094eb8c514ed39ad4d9efa
Berkant Eruysal <75691062+BerkantEruysal@users.noreply.github.com>
2023-08-05 11:46:50 +0300

    Update ServiceConsumerHomeHeader.js

--- src/components/headers/ServiceConsumerHomeHeader.js
+++ src/components/headers/ServiceConsumerHomeHeader.js
@@ -1,14 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import React from 'react';
+import FullLogo from '../logo/FullLogo';
+import colors from '../../utils/styles/DarkTheme';
+import MapButton from '../IconButtons/MapButton';
+import ExploreButton from '../IconButtons/ExploreButton';
+import FakeSearcher from '../searchInputs/FakeSearcher';
 
 const ServiceConsumerHomeHeader = () => {
   return (
-    <View>
-      <Text style={{backgroundColor:"red",height:60}}>ServiceConsumerHomeHeader</Text>
+    <View style={styles.mainContainer}>
+      <SafeAreaView style={styles.safeAreaContainer}>
+        <View style={styles.topPartContainer}>
+          <MapButton></MapButton>
+          <FullLogo></FullLogo>
+          <ExploreButton></ExploreButton>
+        </View>
+        <View style={styles.bottomPartContainer}>
+          <FakeSearcher></FakeSearcher>
+        </View>
+      </SafeAreaView>
     </View>
-  )
-}
+  );
+};
 
-export default ServiceConsumerHomeHeader
+export default ServiceConsumerHomeHeader;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mainContainer: {
+    backgroundColor: colors.background,
+  },
+  safeAreaContainer: {},
+  topPartContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    padding: 20,
+  },
+  bottomPartContainer: {},
+});
