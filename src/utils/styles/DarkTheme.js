755eabf1403370eec770acfb1d26e3d4b54fba82
Berkant Eruysal <75691062+BerkantEruysal@users.noreply.github.com>
2023-08-05 11:46:37 +0300

    Create dark theme color palette

---
+++ src/utils/styles/DarkTheme.js
@@ -0,0 +1,10 @@
+const colors = {
+    primary: '#F05454',
+    secondary: '#30475E',
+    text: '#FFFFFF',
+    secondaryText: '#C9C9C9',
+    background: '#222831',
+    secondaryBackground: '#101317',
+}
+
+export default colors;
