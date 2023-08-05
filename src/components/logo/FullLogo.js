2f341b10b3517b783d15bcc4315480a6025cd92b
Berkant Eruysal <75691062+BerkantEruysal@users.noreply.github.com>
2023-08-05 11:46:17 +0300

    Create FullLogo.js

---
+++ src/components/logo/FullLogo.js
@@ -0,0 +1,14 @@
+import { StyleSheet, View, Image } from 'react-native'
+import React from 'react'
+
+const FullLogo = () => {
+  return (
+    <View>
+      <Image source={require('../../assets/logo/FullLogoWhite.png')}></Image>
+    </View>
+  )
+}
+
+export default FullLogo
+
+const styles = StyleSheet.create({})
