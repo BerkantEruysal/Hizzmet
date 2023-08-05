f99947382f11eb59ffe09c420480bd66ba81e936
Berkant Eruysal <75691062+BerkantEruysal@users.noreply.github.com>
2023-08-05 11:46:44 +0300

    Update Home.js

--- src/screens/serviceConsumer/Home.js
+++ src/screens/serviceConsumer/Home.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import colors from '../../utils/styles/DarkTheme'
 
 const Home = () => {
   return (
@@ -14,5 +15,6 @@ export default Home
 const styles = StyleSheet.create({
   mainContainer:{
     flex:1,
+    backgroundColor: colors.secondaryBackground
   }
 })
