66ee3cc73e2950878a58d6d148063b4ce1e13bc6
Berkant Eruysal <75691062+BerkantEruysal@users.noreply.github.com>
2023-08-12 00:01:54 +0300

    Update ServiceConsumerExplore.js

--- src/navigation/ServiceConsumerExplore.js
+++ src/navigation/ServiceConsumerExplore.js
@@ -1,14 +1,19 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import ServiceProviderProfile from '../screens/serviceConsumer/ServiceProviderProfile';
+
+const Stack = createNativeStackNavigator();
 
 const ServiceConsumerExplore = () => {
   return (
-    <View>
-      <Text>ServiceConsumerExplore</Text>
-    </View>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen
+        name="ServiceProviderProfile"
+        component={ServiceProviderProfile}
+      />
+    </Stack.Navigator>
   );
 };
 
 export default ServiceConsumerExplore;
-
-const styles = StyleSheet.create({});
