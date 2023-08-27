b481c8a1583d5df9183f2569ae82307851778465
Berkant Eruysal <75691062+BerkantEruysal@users.noreply.github.com>
2023-08-27 13:11:28 +0300

    Create ServiceConsumerMessageDetailHeader.js

---
+++ src/components/headers/ServiceConsumerMessageDetailHeader.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const ServiceConsumerMessageDetailHeader = () => {
+  return (
+    <View style={styles.mainContainer}>
+      <SafeAreaView></SafeAreaView>
+    </View>
+  );
+};
+
+export default ServiceConsumerMessageDetailHeader;
+
+const styles = StyleSheet.create({});
